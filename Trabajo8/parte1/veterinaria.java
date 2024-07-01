3ae6362806c82970bdf15bb723abe93ba83b19de
PalaciosJoaquin <109171834+PalaciosJoaquin@users.noreply.github.com>
2024-07-01 18:36:24 -0300

    Add files via upload

---
+++ Trabajo8/parte1/veterinaria.java
@@ -0,0 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/veterinaria";
+    private static final String USER = "root";
+    private static final String PASSWORD = "tu_contraseña";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
